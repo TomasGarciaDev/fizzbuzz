06d0a7ff84bb088b6b41bf35c0b4345e4b91c9e0
Tomas Garcia <tomasgarciadev@gmail.com>
2021-11-09 14:00:02 +0000

    full fizzbuzz.rb and spec

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,3 @@
 def fizzbuzz(number)
-  'fizz'
+  number % 3 == 0 && number % 5 == 0 ? "fizzbuzz" : (number % 3 == 0 ? "fizz" : (number % 5 == 0 ? "buzz" : number))
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,5 +3,6 @@ require 'fizzbuzz'
 describe 'fizzbuzz' do
   it 'returns "fizzbuzz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  
   end
 end
